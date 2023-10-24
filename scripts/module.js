a276f9d85be0f95e749a546082c84d7957aa966d
cragunbc <bccragun@yahoo.com>
2023-10-23 19:18:23 -0600

    Final Project

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,62 @@
-import displayPlayers from "./project.js";
+const playersElement = document.getElementById('grid');
+let playerList = [];
+
+let pageTitle = "2023 - 2024 Utah Jazz Roster";
+const pageElement = document.getElementById("pagetitle");
+pageElement.innerHTML = `<strong>${pageTitle}<strong>`;
+
+
+const displayPlayers = (players) => {
+    players.forEach((player) => {
+        let article = document.createElement("article");
+        let playerName = document.createElement("h3");
+        let playerPosition = document.createElement("h4");
+        let playerCountry = document.createElement("h4");
+        let playerNumber = document.createElement("h4");
+        let playerPoints = document.createElement("h4");
+        let playerRebounds = document.createElement("h4");
+        let playerAssists = document.createElement("h4");
+        let yearsPro = document.createElement("h4");
+        let image = document.createElement("img");
+        image.setAttribute("src", player.imageUrl);
+        image.setAttribute("alt", player.playerName);
+        playerName.innerHTML = `<strong>${playerName.textContent = player.playerName}<strong>`;
+        playerPosition.innerHTML = `<strong>Position: ${playerPosition.textContent = player.position}<strong>`;
+        playerCountry.innerHTML = `<strong>Player Country: ${playerCountry.textContent = player.country}<strong>`;
+        playerNumber.innerHTML = `<strong>Player Number: ${playerNumber.textContent = player.number}<strong>`;
+        playerPoints.innerHTML = `<strong>Points Per Game: ${playerPoints.textContent = player.pointsPerGame}<strong>`;
+        playerRebounds.innerHTML = `<strong> Rebounds Per Game: ${playerRebounds.textContent = player.reboundsPerGame}<strong>`;
+        playerAssists.innerHTML = `<strong>Assists Per Game: ${playerAssists.textContent = player.assistsPerGame}<strong>`;
+        yearsPro.innerHTML = `<strong>Years Pro: ${yearsPro.textContent = player.yearsPro}<strong>`;
+        article.appendChild(playerName);
+        article.appendChild(image);
+        article.appendChild(playerNumber);
+        article.appendChild(playerPosition);
+        article.appendChild(playerCountry);
+        article.appendChild(playerPoints);
+        article.appendChild(playerRebounds);
+        article.appendChild(playerAssists);
+        article.appendChild(yearsPro);
+
+        playersElement.appendChild(article);
+
+        document.querySelector("#players").appendChild(article);
+    });
+};
+
+
+const getPlayers = async () => {
+    const response = await fetch("https://run.mocky.io/v3/7fffef5c-e9ef-4661-a286-302af8d89edd");
+    playerList = await response.json();
+    displayPlayers(playerList);
+};
+
+//getPlayers();
+
+
 
 const reset = () => {
-    document.querySelector("#players").innerHTML = "";
+     document.querySelector("#players").innerHTML = "";
 };
 
 const sortBy = (players) => {
@@ -87,3 +142,5 @@ const sortBy = (players) => {
 //getPlayers();
 
 document.querySelector("#sortBy").addEventListener("change", () => {sortBy(playerList) });
+
+export default getPlayers;
